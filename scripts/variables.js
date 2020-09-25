78be24eb51e09d7c4e251b0fe138ddceec396b5b
Adsmart22 <silva.ad.m10@gmail.com>
2020-09-24 23:49:08 -0500

    cambio a https

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,9 +1,9 @@
 export const apiKey = "fIqIZpCyt2zeZ9jE83dOA4jVd5Du3GIK";
-export const endpointTrending = "http://api.giphy.com/v1/gifs/trending";
-export const endpointTrendingTag = "http://api.giphy.com/v1/trending/searches";
-export const endpointSearch = "http://api.giphy.com/v1/gifs/search";
-export const endpointAutocomplete = "http://api.giphy.com/v1/gifs/search/tags";
-export const endpointGifById = "http://api.giphy.com/v1/gifs/";
+export const endpointTrending = "https://api.giphy.com/v1/gifs/trending";
+export const endpointTrendingTag = "https://api.giphy.com/v1/trending/searches";
+export const endpointSearch = "https://api.giphy.com/v1/gifs/search";
+export const endpointAutocomplete = "https://api.giphy.com/v1/gifs/search/tags";
+export const endpointGifById = "https://api.giphy.com/v1/gifs/";
 export const tituloGenerico = "GIF animado";
 export const nombreGenerico = "GIF";
 export let arregloFavoritos = [];
@@ -14,7 +14,7 @@ export let constraints = {
         height: 320,
     } 
 };
-export const endpointCargar = "http://upload.giphy.com/v1/gifs";
+export const endpointCargar = "https://upload.giphy.com/v1/gifs";
 export function limpiarGrid(nameObject) {
     let grid = document.getElementById(nameObject);
     let panelNoFav = document.getElementById("noFavoritos");
